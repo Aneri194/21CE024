4e27ba843611083b4a34124db1bc5f928f5cb1fb
21CE024 <110349433+Aneri194@users.noreply.github.com>
2022-10-08 19:24:00 +0530

    Add files via upload

---
+++ Maiin.java
@@ -0,0 +1,32 @@
+//21CE024_Aneri
+package Assignment5;
+import java.io.*;
+class FileName implements FilenameFilter {
+    String initials;
+    public FileName(String initials)
+    {
+        this.initials = initials;
+    }
+    public boolean accept(File dir, String name)
+    {
+        return name.startsWith(initials);
+    }
+}
+public class Maiin {   
+    public static void main(String[] args)
+    {
+        File directory = new File("/home/user/");
+        FileName filter = new FileName("file.cpp");
+        String[] flist = directory.list(filter);
+ 
+        if (flist == null) {
+            System.out.println(
+                "Empty directory or directory does not exists.");
+        }
+        else {
+            for (int i = 0; i < flist.length; i++) {
+                System.out.println(flist[i]+" found");
+            }
+        }
+    }
+}
