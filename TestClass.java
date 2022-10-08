5df9c49c0535244f078054e65e3beab0e38adfc0
21CE024 <110349433+Aneri194@users.noreply.github.com>
2022-10-08 19:29:54 +0530

    Add files via upload

---
+++ TestClass.java
@@ -0,0 +1,22 @@
+//21CE024_Aneri
+package Assignment5;
+import java.io.*;
+public class TestClass{
+  public static void main(String[] args) {
+    FileInputStream fis = null;
+    FileOutputStream fos = null;
+    try {
+      fis = new FileInputStream("in.txt");
+      fos = new FileOutputStream ("out.txt");
+      int temp;
+      while ((temp = fis.read()) != -1) 
+      fos.write((byte)temp);
+      if (fis != null)
+      fis.close();
+      if (fos != null)
+      fos.close();
+    }catch(Exception e){
+      System.out.println(e);
+    }
+  }
+}
