28ff240701c21101cf01f088f1d91799f096f325
21CE024 <110349433+Aneri194@users.noreply.github.com>
2022-10-08 19:10:29 +0530

    Add files via upload

---
+++ FileCreator.java
@@ -0,0 +1,19 @@
+//21CE024_Aneri
+package Assignment5;
+import java.io.IOException;
+import java.nio.file.Files;  
+import java.nio.file.Path;
+import java.nio.file.Paths;
+public class FileCreator{
+	public static void main(String... args) throws IOException{
+		System.out.println("Using createFile() method: ");
+		Path path = Paths.get("newFile.txt");
+		try{
+			Files.createFile(path);
+			System.out.println("File Created: " + path);
+		}
+		catch(FileAlreadyExistsException e){
+			System.out.println("File already exists at Path: " + path);
+		}
+         }
+}
